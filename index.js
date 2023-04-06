a6aca08b264f83de04e0555edb585cc7965ab267
phunghung0901xx <phunghung0901xx@gmail.com>
2023-04-06 21:52:29 +0700

    update

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(express.json());
+app.use(cors({ origin: true }));
+const axios = require("axios");
+
+app.post("/authenticate", async (req, res) => {
+    const { username } = req.body;
+    // Get or create user on Chat Engine!
+    try {
+        const r = await axios.put(
+            "https://api.chatengine.io/users/",
+            { username: username, secret: username, first_name: username },
+            { headers: { "Private-Key": "6a9f3438-f1f2-4017-871b-9a2bd923841e" } }
+        );
+        return res.status(r.status).json(r.data);
+    } catch (e) {
+        return res.status(e.response.status).json(e.response.data);
+    }
+});
+
+app.listen(3001);
